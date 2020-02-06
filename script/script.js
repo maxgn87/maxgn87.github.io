40e9e153a5d2c61440ead37d25d108d7171b77b8
maxgn87 <60128547+maxgn87@users.noreply.github.com>
2020-02-06 15:34:53 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -240,16 +240,14 @@
 
 $(document).ready(function () {
 
-var top_show = 400; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
-  var delay = 1000; // Задержка прокрутки
+var top_show = 400; 
+  var delay = 1000; 
   $(document).ready(function() {
-    $(window).scroll(function () { // При прокрутке попадаем в эту функцию
-      /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
+    $(window).scroll(function () {
       if ($(this).scrollTop() > top_show) $('.top_btn').fadeIn();
       else $('.top_btn').fadeOut();
     });
-    $('.top_btn').click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
-      /* Плавная прокрутка наверх */
+    $('.top_btn').click(function () { 
       $('body, html').animate({
         scrollTop: 0
       }, delay);
